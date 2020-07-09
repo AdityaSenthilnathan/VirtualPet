71907cc55b17ae5b6bdde8729d2c0e283f7a9107
AdityaSenthilnathan <66099566+AdityaSenthilnathan@users.noreply.github.com>
2020-07-09 01:09:54 -0700

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,67 @@
+class Food{
+    constructor(x,y, foodStock, lastfed){
+        this.milkImage = loadImage("images/Milk.png");
+        this.body = Matter.Bodies.rectangle(x,y,20,20);
+
+    }
+
+    display(){
+        var x = 0;
+        var y = 0;
+for (var i = 1; i <= foodS; i++){
+
+
+        image(this.milkImage, this.body.position.x + x, this.body.position.y + y, 100,100);
+        x = x + 40
+        if (i === 10){
+            y = y + 50
+            x = 0
+        }
+  
+}
+       
+
+    }
+
+getFoodStock (){
+
+    foodStock = firebase.database().ref("Food");
+}
+
+updateFoodStock(x){
+       
+    if (x >= 20) {
+        x = 20;
+      }
+      else {
+        x = x + 1;
+      }
+    firebase.database().ref('/').update({
+        Food: x
+      });
+
+}
+
+deductFood(x){
+    
+    if (x <= 0) {
+        x = 0;
+      }
+      else {
+        x = x - 1;
+      }
+    firebase.database().ref('/').update({
+      Food: x
+    }); 
+    
+}
+
+  
+ 
+
+
+
+
+
+
+}
---
+++ sketch.js
@@ -0,0 +1,101 @@
+var dogIMG, dogIMG2, dog, foodS, foodStock, dataBase,  foodObj;
+var feedTime = 0; 
+var feedT = "feed the dog to see its last feed later";
+var hour;
+var hourmin;
+
+  
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const db = firebase.database().ref();
+foodS = 0;
+function preload() {
+  dogIMG = loadImage("images/dogImg.png");
+  dogIMG2 = loadImage("images/dogImg1.png");
+  //dataBase = firebase.database();
+  //db = firebase.database().ref();
+}
+
+function setup() {
+  createCanvas(1000, 500);
+  foodObj = new Food(10,200);
+  dog = createSprite(650, 250, 10, 10);
+  dog.addImage(dogIMG);
+  dog.scale = .1;
+  textSize(30);
+  fill("green");
+  stroke(10);
+  feed = createButton("Feed The Dog");
+  feed.position(800, 75);
+
+  restock = createButton("Restock your food");
+  restock.position(920, 75);
+  feedTime = firebase.database().ref("FeedTime");
+feedTime.on("value", readTime);
+
+}
+
+
+function draw() {
+
+  background(46, 139, 87);
+  drawSprites();
+  gettime();
+
+//feedT = feedTime.val();
+
+  feed.mousePressed(feedDog); 
+  restock.mousePressed(restockFood);
+  
+  foodObj.display();
+
+  foodObj.getFoodStock();
+  foodStock.on("value", readStock);
+
+  text("Food Avalible: " + foodS, 20, 55);
+
+
+ if (feedT>=12){
+  text("Last Fed : " + feedT + "PM", 20,85); 
+
+ }else if(feedT == 0){
+  text("Last Fed : 12 AM" , 20,85); 
+
+ } else {
+  text("Last Fed : " + feedT + "AM", 20,85); 
+
+}
+}
+
+function readStock(data) {
+  foodS = data.val();
+}
+function readTime(data) {
+  feedT = data.val();
+}
+function restockFood() {
+  dog.addImage(dogIMG);
+  foodObj.updateFoodStock(foodS);
+}
+
+function feedDog(){
+  feedT = hourmin;
+  firebase.database().ref('/').update({
+    FeedTime: feedT
+  });
+
+  foodObj.deductFood(foodS);
+  dog.addImage(dogIMG2);
+}
+
+async function gettime (){
+  var response = await fetch("http://worldtimeapi.org/api/timezone/America/Los_Angeles")
+  var time = await response.json();
+   var dateTime = time.datetime;
+  hour = dateTime.slice(11,13);
+  hourmin = dateTime.slice(11,13);
+}
+
+
